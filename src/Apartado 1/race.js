70a3e942ee510f9190830f6c4c78c3d7f07e8e6a
Alvaro Maceda <alvaro@alvaromaceda.es>
2023-05-04 16:39:51 +0200

    Templates for the exam

---
+++ src/Apartado 1/race.js
@@ -0,0 +1,30 @@
+const MIN_TIMEOUT = 500; // milisegundos
+const MAX_TIMEOUT = 1500; // milisegundos
+const MIN_STEPS = 3;
+const MAX_STEPS = 10;
+const PAUSE = 100 // milisegundos
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function randomRun(){
+  /*
+  Your code here
+  */
+}
+
+function pausa(interval) {
+  /*
+  Your code here
+  */
+}
+
+function race(length, step, finish) {
+  /*
+  Your code here
+  */
+}
+
+
