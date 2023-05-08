0e4cdfe4e58fbaf3e7cc9c982484733ba3a33396
Alvaro <alvaro@alvaromaceda.es>
2023-05-08 16:51:04 +0200

    Adds missing colors.js file

---
+++ src/Apartado 2/colors.js
@@ -0,0 +1,24 @@
+const COLORS = [
+  '#00ffff', //aqua
+  '#ff7f50', //coral
+  '#ffd700', //gold
+  '#ff69b4', //hotpink
+  '#7fff00', //chartreuse
+  '#00ff00', //lime
+  '#ffa500', //orange
+  '#ff0000', //red
+  '#87ceeb', //skyblue
+  '#ffff00', //yellow
+  '#0000ff', //blue
+  '#008000', //green
+  '#ffc0cb', //pink
+  '#800080', //purple
+  '#ff00ff', //magenta
+  '#40e0d0', //turquoise
+  '#ee82ee', //violet
+  '#fa8072', //salmon
+  '#d2b48c', //tan
+  '#ff6347' //tomato
+];
+
+export { COLORS }
